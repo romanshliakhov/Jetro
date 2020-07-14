e012b5add179c39afe2994dcc306d702d5181e39
romanshliakhov <romanshliakhov@gmail.com>
2020-07-14 16:04:21 +0300

    added adaptive

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ $(function(){
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/next.png" alt=""></button>',
         autoplay: 3000,
         fade: true,
-        asNavFor: '.preview',                       
+        asNavFor: '.preview',   
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    arrows: false,
+                    autoplay: 1000,
+                }
+            }
+        ]                    
     }); 
 
     $('.preview').slick({
@@ -13,9 +22,12 @@ $(function(){
         slidesToScroll: 6,  
         asNavFor: '.slider__inner',    
         focusOnSelect: true,           
-        variableWidth: true,   
-        
+        variableWidth: true,                
     }); 
+
+    $('.header__menu-btn').on('click',function(){
+        $('.header__list').slideToggle();
+    });
 });
 
 
